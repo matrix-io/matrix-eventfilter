08345fdcb17fed989770a382a164fba6242bbb6b
Sean Canton <eighteyes@gmail.com>
2015-06-19 17:25:54 -0400

    Tests working

--- index.js
+++ index.js
@@ -224,6 +224,8 @@ module.exports = {
   token : authToken
 };
 
+var applyFilter = require('./filter.js')
+
 function testSocket(){
   var socket = new net.Socket();
   var BASELINE = 100;
@@ -261,85 +263,4 @@ face.has('age').between(10,35).and.is('sex', 1)
   })
 }
 
-function applyFilter( filter, object ){
-  var pass = false;
-  console.log('Filter', filter, object );
-  if ( object.type === filter.eventName) {
-  for (var k in object){
-    // filter object
-
-    var objValue = object[k];
-    // the specific filter
-    var f = _.filter(filter.filters, function(f){
-      return _.has( f, k );
-    })[0];
-
-
-    if ( k !== 'type' && typeof f !== 'undefined' ){
-    console.log('=', k, f);
-
-      var testValue = f[k];
-      // console.log('===', f, k)
-      // console.log('===', objValue, '<=>', testValue );
-      // a filter exists
-      if ( _.isPlainObject(testValue)){
-        //complex filter
-        for (var fk in f) {
-          var subpass = [];
-          var actions = f[fk];
-
-          for (var fa in actions) {
-            var fav = actions[fa];
-
-            //filter action value
-
-            console.log('====', f, '|', fa, '::', fav);
-            if (fa == '$lte') {
-              subpass.push((objValue <= fav))
-            }
-            if (fa === '$gte') {
-              subpass.push((objValue >= fav))
-            }
-            if (fa === '$match') {
-              subpass.push(objValue.search(/+fav+/).length > 0)
-            }
-
-          }
-
-          if ( subpass.length > 0 ){
-            console.log('==== subpass', subpass);
-            pass = _.reduce(subpass, function(o, v) {
-              return (o && v);
-            });
-            console.log('==== pass', pass);
-          }
-        }
-      } else {
-        //simple filter
-        if ( !isNaN( objValue ) ){
-          if ( objValue < 1 && objValue > 0){
-            // confidence
-            objValue *= 100;
-          }
-          // numerical object value
-          if ( !isNaN( testValue )){
-            //filter looking for an integer
-            console.log('===', testValue, '<=>', objValue )
-            pass = ( testValue == objValue );
-          } else {
-
-
-          }
-
-        }
-        //
-      }
-    }
-
-  }
-}
-
-  console.log('pass', pass);
-  return (pass) ? object : null;
-}
 // testSocket();
--- test.js
+++ test.js
@@ -1,49 +1,53 @@
 var should = require('should');
 var events = require('./');
+var applyFilter = require('./filter.js')
 
 
-var StreamFilter;
-describe('EventFilter instance', function() {
+var StreamFilter = new events.StreamFilter('test');
+describe('EventFilter', function() {
 
   before(function(done) {
     this.timeout = 10000;
-    StreamFilter = new events.StreamFilter('test');
     process.env.NODE_ENV = "development";
     events.init("L04BU70GJU5S49JLNR783UK4UZSYWK0L", "Y7SRP3J1TDOQ49H4GP6AE3C35WXYN9OQ", done);
   });
 
-  beforeEach(function(done) {
-    StreamFilter.clear();
-    done();
-  })
 
-  it('should make thing', function(done) {
-    (StreamFilter.eventName).should.ok;
-    done();
-  });
+  describe('instance methods', function(){
 
-  it.skip('should initialize and have a token', function(done) {
-    StreamFilter.token.should.ok;
-    done();
-  });
-
-  it('should do is() or like() to add filters', function(done) {
-    StreamFilter.is('dog', true).like('cat', false)
-    StreamFilter.filters.should.containDeep([{
-      dog: true
-    }]);
-    done();
-  });
-
-
-  it('should support object constructors', function(done) {
-    StreamFilter.is({
-      crime: false,
-      falselyAccused: true,
-      yearsFacing: 10
+    beforeEach(function(done) {
+      StreamFilter.clear();
+      done();
     })
-    StreamFilter.filters.should.length(3);
-    done();
+
+    it('should make thing', function(done) {
+      (StreamFilter.eventName).should.ok;
+      done();
+    });
+
+    it.skip('should initialize and have a token', function(done) {
+      StreamFilter.token.should.ok;
+      done();
+    });
+
+    it('should do is() or like() to add filters', function(done) {
+      StreamFilter.is('dog', true).like('cat', false)
+      StreamFilter.filters.should.containDeep([{
+        dog: true
+      }]);
+      done();
+    });
+
+
+    it('should support object constructors', function(done) {
+      StreamFilter.is({
+        crime: false,
+        falselyAccused: true,
+        yearsFacing: 10
+      })
+      StreamFilter.filters.should.length(3);
+      done();
+    });
   });
 
   describe('conditional objects', function() {
@@ -120,6 +124,52 @@ describe('EventFilter instance', function() {
       StreamFilter.filters.should.containDeep([{age: 18 }]);
       done();
     });
+  });
+
+
+  describe('filter stream objects', function(){
+    var streamObj;
+    before(function(done){
+      streamObj = {
+        type: 'test',
+        age: 18,
+        glasses: 0.55,
+        beard: 0.23,
+        sex: 1,
+        name: 'Goat'
+      }
+      done();
+    });
+
+    beforeEach(function(done){
+      StreamFilter.clear();
+      done();
+    })
+
+    it('should filter direct integer matches', function(done){
+      var filter = StreamFilter.is('age', 18).val()
+      applyFilter(filter, streamObj).should.property('type');
+      done();
+    });
+
+    it('should filter less / over / between', function(done) {
+      var filter = StreamFilter.has('age').between(10,20).val();
+      applyFilter(filter, streamObj).should.property('type');
+      done();
+    });
+
+    it('should filter direct matches', function(done){
+      var filter = StreamFilter.contains('name','Goat').val();
+      applyFilter(filter, streamObj).should.property('type');
+      done();
+    });
+
+    it('should filter not', function(done){
+      var filter = StreamFilter.not('name','Swan').val();
+      applyFilter(filter, streamObj).should.property('type');
+      done();
+    });
+
   });
 
 });
